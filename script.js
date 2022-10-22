18deb4eed7d113aa517f974c6f479f2470decea7
Caleb Falls <113200588+FallsCaleb@users.noreply.github.com>
2022-10-22 00:32:49 +0000

    I am finished with page 1. Working on the canvas.

--- script.js
+++ script.js
@@ -39,3 +39,14 @@ function validate(){
     
 }
 
+window.onload = draw; // Execute draw function when DOM is ready
+function draw() {
+     // Assign our canvas element to a variable 
+    var canvas = document.getElementById("canvas1");
+    // Check to see if the canvas is supported in the user broswer
+    if(canvas.getContext){
+        var ctx = canvas.getContext("2d"); 
+        ctx.fillStyle = "rgba(24,124,200,1)";
+        ctx.fillRect (36, 10, 40, 40);
+    }
+}
